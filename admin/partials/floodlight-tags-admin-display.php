e36698f95e8b065d6e8fa0e2d656cb315a74f268
pjsinco <psinco@fas.harvard.edu>
2015-10-28 09:04:37 -0500

    First commit

---
+++ admin/partials/floodlight-tags-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       http://github.com/pjsinco
+ * @since      1.0.0
+ *
+ * @package    Floodlight_Tags
+ * @subpackage Floodlight_Tags/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
